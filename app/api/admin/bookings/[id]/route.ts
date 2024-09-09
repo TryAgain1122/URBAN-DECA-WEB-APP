6c58c587715da40955f2682247987c5504453849
TryAgain1122 <luisrafhael1122@gmail.com>
2024-09-09 21:45:34 +0800

    added promise

--- app/api/admin/bookings/[id]/route.ts
+++ app/api/admin/bookings/[id]/route.ts
@@ -1,7 +1,7 @@
 import { deleteBooking } from "@/backend/controllers/bookingControllers";
 import { authorizeRoles, isAuthenticatedUser } from "@/backend/middlewares/auth";
 import { createEdgeRouter } from "next-connect"
-import { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 interface RequestContext {
     params: {
@@ -13,7 +13,7 @@ const router = createEdgeRouter<NextRequest, RequestContext>();
 
 router.use(isAuthenticatedUser, authorizeRoles("admin")).delete(deleteBooking);
 
-export default async function DELETE(request:NextRequest, ctx:RequestContext) {
-    return router.run(request, ctx);
+export async function DELETE(request:NextRequest, ctx:RequestContext): Promise<NextResponse> {
+    return router.run(request, ctx) as Promise <NextResponse>;
 }
 

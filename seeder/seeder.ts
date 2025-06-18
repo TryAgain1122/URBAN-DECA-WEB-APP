d94ed5c37e7a31326ee14d334dc0f5d48e8ce4a0
TryAgain1122 <luisrafhael1122@gmail.com>
2025-06-19 01:03:17 +0800

    seeder comment

--- seeder/seeder.ts
+++ seeder/seeder.ts
@@ -1,23 +1,23 @@
-import Room from "../backend/models/room";
-import mongoose from "mongoose";
-import { rooms } from "./data";
-// require('dotenv').config({ path: 'next.config.js' })
+// // import Room from "../backend/models/room";
+// import mongoose from "mongoose";
+// import { rooms } from "./data";
+// // require('dotenv').config({ path: 'next.config.js' })
 
-const seedRooms = async () => {
-  try {
-    await mongoose.connect("mongodb+srv://rafhael:rafhael@cluster0.80zsjbk.mongodb.net/urbanDb_updated");
+// const seedRooms = async () => {
+//   try {
+//     await mongoose.connect("mongodb+srv://rafhael:rafhael@cluster0.80zsjbk.mongodb.net/urbanDb_updated");
 
-    await Room.deleteMany();
-    console.log("Rooms are deleted");
+//     await Room.deleteMany();
+//     console.log("Rooms are deleted");
 
-    await Room.insertMany(rooms);
-    console.log("Rooms are added");
+//     await Room.insertMany(rooms);
+//     console.log("Rooms are added");
 
-    process.exit();
-  } catch (error) {
-    console.log(error);
-    process.exit();
-  }
-};
+//     process.exit();
+//   } catch (error) {
+//     console.log(error);
+//     process.exit();
+//   }
+// };
 
-seedRooms();
+// seedRooms();
